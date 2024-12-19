1fffdcc4da87d38209644f199b90a3b0ff04091d
DhanarajHuli5 <dhanarajhuli5@gmail.com>
2024-12-20 00:30:10 +0530

    implement user authentication middleware; refactor profile route to use auth and remove unused endpoints

--- devTinder-Backend/src/app.js
+++ devTinder-Backend/src/app.js
@@ -8,6 +8,7 @@ const { default: isEmail } = require("validator/lib/isEmail");
 const validator = require("validator")
 const cookieParser = require("cookie-parser"); // Import cookie-parser
 const jwt = require("jsonwebtoken")
+const { userAuth} = require("./middlewares/auth")
 
 app.use(express.json())
 app.use(cookieParser()); // Use cookie-parser middleware
@@ -31,28 +32,6 @@ app.post("/signup", async (req,res) => {
     } catch (error) {
       res.status(500).send("ERROR " + error.message)
     }
-})
-
-// Get user by email
-app.get("/user", async (req,res) => {
-  const userEmail = req.body.emailId;
-  
-  try{
-    const user = await User.findOne({emailId: userEmail})
-    res.send(user);
-  } catch (error) {
-    res.status(400).send("Something went wrong")
-  }
-})
-
-// Feed API - GET /feed - get all users from the database
-app.get("/feed", async (req,res) => {
-  try{
-    const user = await User.find({})
-    res.send(user);
-  } catch (error) {
-    res.status(400).send("Something went wrong")
-  }
 })
 
 // Login API
@@ -83,60 +62,13 @@ app.post("/login", async (req,res) => {
   }
 })
 
-// Delete user
-app.delete("/user", async (req,res) => {
-  const userId = req.body.userId; 
-  try{
-    const user = await User.findByIdAndDelete(userId)
-    res.send(user);
-  } catch (error) {
-    res.status(400).send("Something went wrong")
-  }
-})
-
 // Profile 
-app.get("/profile", async (req, res) => {
+app.get("/profile",userAuth,  async (req, res) => {
   try {
-    const cookies = req.cookies;
-    const { token } = cookies;
-    if(!token){
-      throw new Error("Invalid Token");
-    }
-    const decodedMessage = await jwt.verify(token, "secret");
-    const { userId } = decodedMessage;
-    const user = await User.findById(userId)
-    if(!user){
-      throw new Error("User not exits")
-    }
+    const user = req.user;
     res.send(user);
   } catch (error) {
     res.status(401).send("Authentication failed: " + error.message);
-  }
-})
-
-// Update user
-app.patch("/user/:userId", async (req,res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try{
-    //allowed updates
-    const ALLOWED_UPDATES =["photoUrl","about","gender","age","skills"];
-
-    const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k)
-    )
-
-    if(!isUpdateAllowed){
-      throw new Error("Update not allowed he he");
-    }
-
-    const user = await User.findByIdAndUpdate({_id: userId}, data, {
-      returnDocument:"after",
-      runValidators: true,
-    })
-    res.send("User updated successfully")
-  } catch(error){
-    res.status(400).send("Error is " + error.message);
   }
 })
 
