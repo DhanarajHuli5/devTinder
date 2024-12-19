7ceee71cdc80e88bc12e9db27149d09fc7ebae1b
DhanarajHuli5 <dhanarajhuli5@gmail.com>
2024-12-19 21:49:12 +0530

    add cookie-parser and jsonwebtoken for authentication; implement JWT token handling in login and profile routes

--- devTinder-Backend/src/app.js
+++ devTinder-Backend/src/app.js
@@ -6,9 +6,12 @@ const { validateSignUpData} = require("./utils/validation")
 const bcrypt = require("bcrypt");   
 const { default: isEmail } = require("validator/lib/isEmail");
 const validator = require("validator")
-
+const cookieParser = require("cookie-parser"); // Import cookie-parser
+const jwt = require("jsonwebtoken")
 
 app.use(express.json())
+app.use(cookieParser()); // Use cookie-parser middleware
+
 app.post("/signup", async (req,res) => {
   // creating a new instance of User model
   try {
@@ -20,36 +23,26 @@ app.post("/signup", async (req,res) => {
       // Encrypt the password
       const passwordHash = await bcrypt.hash(password,10)
 
-
-
-      const user = new User({
+      const user = new User({ 
         firstName,lastName, emailId, password: passwordHash
       });
       await user.save()
       res.send("User added successfully")
     } catch (error) {
-      console.log(error)
       res.status(500).send("ERROR " + error.message)
     }
-   
-    
 })
-
 
 // Get user by email
 app.get("/user", async (req,res) => {
   const userEmail = req.body.emailId;
   
-   
   try{
     const user = await User.findOne({emailId: userEmail})
-    console.log(user);
-    
     res.send(user);
   } catch (error) {
     res.status(400).send("Something went wrong")
   }
-  
 })
 
 // Feed API - GET /feed - get all users from the database
@@ -65,7 +58,6 @@ app.get("/feed", async (req,res) => {
 // Login API
 app.post("/login", async (req,res) => {
   try{
-
     const { emailId , password } = req.body;
     if(!validator.isEmail(emailId)){
       throw new Error("Invalid emailId")
@@ -79,14 +71,13 @@ app.post("/login", async (req,res) => {
     const isPasswordValid = await  bcrypt.compare(password,user.password)
 
     if(isPasswordValid){
+      //JWT token create
+      const token = await jwt.sign({userId: user._id}, "secret")
+      res.cookie("token",token) 
       res.send("Login successfull !!")
     } else {
       throw new Error("Invalid credentials")
     }
-
-
-
-
   } catch (err){
     res.status(400).send("ERROR : " + err.message)
   }
@@ -103,13 +94,32 @@ app.delete("/user", async (req,res) => {
   }
 })
 
+// Profile 
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if(!token){
+      throw new Error("Invalid Token");
+    }
+    const decodedMessage = await jwt.verify(token, "secret");
+    const { userId } = decodedMessage;
+    const user = await User.findById(userId)
+    if(!user){
+      throw new Error("User not exits")
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(401).send("Authentication failed: " + error.message);
+  }
+})
 
+// Update user
 app.patch("/user/:userId", async (req,res) => {
   const userId = req.params?.userId;
   const data = req.body;
 
   try{
-
     //allowed updates
     const ALLOWED_UPDATES =["photoUrl","about","gender","age","skills"];
 
@@ -124,7 +134,6 @@ app.patch("/user/:userId", async (req,res) => {
       returnDocument:"after",
       runValidators: true,
     })
-    console.log(user);
     res.send("User updated successfully")
   } catch(error){
     res.status(400).send("Error is " + error.message);
@@ -135,7 +144,7 @@ connectDB().then(() => {
   console.log("Database connected...");
   app.listen(3000, () => {
     console.log("Server running on port 3000");
-  });
+  });      
   
 }).catch( err => {
   console.error("Database connection failed");
