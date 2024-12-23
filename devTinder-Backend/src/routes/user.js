ddc809bc4de0e33e8fb8adda32c68bdbb327873f
DhanarajHuli5 <dhanarajhuli5@gmail.com>
2024-12-23 05:42:14 +0530

    feat: add endpoint to fetch accepted connections for the logged-in user

--- devTinder-Backend/src/routes/user.js
+++ devTinder-Backend/src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 const User = require("../models/user");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const { connect } = require("mongoose");
 
 
 // Get all the pending connection request for the logged in user
@@ -25,5 +26,30 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     }
 })
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id, status: "accepted"},
+                {toUserId: loggedInUser._id, status: "accepted"}
+            ]
+        }).populate("fromUserId",["firstName","lastName"]) 
+        const data = connectionRequests.map((row) => row.fromUserId);
+
+
+        res.json({
+            message: "Data fetched successfully",
+            data
+        })
+
+
+
+    } catch (error) {
+        res.status(400).send("ERROR: " + error.message);
+    }
+})
 
 module.exports = userRouter
